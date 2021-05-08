ca01f4163cd14d433a6073f1f6e25b35b532ac60
MatteoGisondi <matteo.gisondi@gmail.com>
2021-05-08 16:24:42 -0400

    Experimental jquery

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 import os
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for, jsonify, request
 from trycourier import Courier
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = 'secret_key'
-app.debug = False
+app.debug = True
 
 client = Courier(auth_token=os.getenv('COURIER_AUTH'))
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    result = a + b
+    print(result)
+    return jsonify(result=result)
 
 @app.route('/')
 def index():
