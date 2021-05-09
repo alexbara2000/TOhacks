5cc2a6c5a238616671423a4822c10764e7613a05
alexandru Bara <alexandrubara2000@gmail.com>
2021-05-09 11:25:41 -0400

    fixed predictions

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def province(province):
     # print(chart_data)
     prediction = []
     for i in range(6):
-        prediction.append(data.predict(df.tail(30), i + 1))
+        prediction.append(data.predict(df.tail(25), i + 1))
     print(prediction)
 
     return render_template('province.html', province=province, chart_data=chart_data, prediction=prediction)
